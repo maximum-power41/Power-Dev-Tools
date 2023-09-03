96db589db12fb0113653b9466ed0b18dcefae667
maximumpower41 <42240837+maximum-power41@users.noreply.github.com>
2023-09-02 23:52:57 -0400

    Initial Commit
    Start of the project :)

---
+++ src/main/java/dev/power/powerdevtools/Power_Dev_Tools.java
@@ -0,0 +1,15 @@
+package dev.power.powerdevtools;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Power_Dev_Tools implements ModInitializer {
+
+
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/dev/power/powerdevtools/client/Power_Dev_ToolsClient.java
@@ -0,0 +1,14 @@
+package dev.power.powerdevtools.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Power_Dev_ToolsClient implements ClientModInitializer {
+
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
